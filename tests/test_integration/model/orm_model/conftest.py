e881bfafe33d5cd515d9f79457febd93c417fe40
Midnighter <midnighter@posteo.net>
2019-12-22 13:52:25 +0100

    test: start ORM integration tests

---
+++ tests/test_integration/model/orm_model/conftest.py
@@ -0,0 +1,74 @@
+# Copyright 2011-2019 Moritz Emanuel Beber
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from brenda_parser.model.orm_model import Base
+
+
+Session = sessionmaker()
+
+
+@pytest.fixture(scope="session")
+def connection():
+    """
+    Use a connection such that transactions can be used.
+
+    Notes
+    -----
+    Follows a transaction pattern described in the following:
+    http://docs.sqlalchemy.org/en/latest/orm/session_transaction.html#session-begin-nested
+
+    """
+    engine = create_engine("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
+    connection = engine.connect()
+    try:
+        yield connection
+    finally:
+        connection.close()
+
+
+@pytest.fixture(scope="function")
+def session(connection):
+    """
+    Create a transaction and session per test unit.
+
+    Rolling back a transaction removes even committed rows
+    (``session.commit``) from the database.
+
+    """
+    transaction = connection.begin()
+    session = Session(bind=connection)
+    try:
+        yield session
+    finally:
+        session.close()
+        transaction.rollback()
